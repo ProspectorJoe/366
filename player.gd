2fd036e3b98317d1be08743828bef64ed2037713
ProspectorJoe <136775318+ProspectorJoe@users.noreply.github.com>
2023-06-16 11:56:53 -0700

    added basic player movement

--- player.gd
+++ player.gd
@@ -1,6 +1,22 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 100
 
 func _physics_process(delta):
+	player_movement(delta)
 	
+
+func player_movement(delta):
+	if Input.is_action_pressed("right"):
+		velocity.x = SPEED
+	elif Input.is_action_pressed("left"):
+		velocity.x = -SPEED
+	else:
+		velocity.x = 0
+	if Input.is_action_pressed("down"):
+		velocity.y = SPEED
+	elif Input.is_action_pressed("up"):
+		velocity.y = -SPEED
+	else:
+		velocity.y = 0
+	move_and_slide()
